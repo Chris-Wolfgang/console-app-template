0e1d06682847c65f16db1e1a1b1bcbf4cb43050b
Chris Wolfgang <196475878+chriswolfgang-test@users.noreply.github.com>
2025-05-11 20:52:33 -0400

    Add appsetting.json support for single environment or multienvironment configuration (Issue #7)

---
+++ ConsoleAppTemplate/ConsoleAppTemplate/Model/SingleEnvironmentConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleAppTemplate.Model;
+
+internal record SingleEnvironmentConfiguration
+{
+    public required string BasePath { get; init; }
+
+    public required ConfigurationFile ConfigFile { get; init; }
+}
