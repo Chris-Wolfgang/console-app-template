7da0e6a81f3f537c478b83d0c118bc284038076f
Chris Wolfgang <196475878+chriswolfgang-test@users.noreply.github.com>
2025-05-16 21:15:33 -0400

    Added logging for unhandled exceptions. (Issues #9)

--- ConsoleAppTemplate/ConsoleAppTemplate/ExitCode.cs
+++ ConsoleAppTemplate/ConsoleAppTemplate/ExitCode.cs
@@ -4,5 +4,6 @@ internal static class ExitCode
 {
 
     public static readonly int Success = 0;
-    public static readonly int ApplicationError = 100;
+    public static readonly int UnhandledException = 10;
+    public static readonly int ApplicationError = 11;
 }
