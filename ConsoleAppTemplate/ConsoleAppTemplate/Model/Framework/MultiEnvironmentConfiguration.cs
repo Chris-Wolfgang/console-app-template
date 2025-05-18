90be9f098c2ff26b7af578d00034772ab14daeb2
Chris Wolfgang <196475878+chriswolfgang-test@users.noreply.github.com>
2025-05-17 22:31:34 -0400

    Removed unused files

--- ConsoleAppTemplate/ConsoleAppTemplate/Model/Framework/MultiEnvironmentConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConsoleAppTemplate.Model.Framework;
-
-internal record MultiEnvironmentConfiguration
-{
-    public required string BasePath { get; init; }
-
-    public required ConfigurationFile ConfigFile { get; init; }
-}
