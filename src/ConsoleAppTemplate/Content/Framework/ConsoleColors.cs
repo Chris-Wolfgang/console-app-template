2c5605e32913c597521a726d4132c09354974326
Chris Wolfgang <196475878+chriswolfgang-test@users.noreply.github.com>
2025-06-01 20:20:11 -0400

    Added console color strings (Issue #11)

---
+++ src/ConsoleAppTemplate/Content/Framework/ConsoleColors.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleAppTemplate.Framework
+{
+    internal static class ConsoleColors
+    {
+        // https://ss64.com/nt/syntax-ansi.html
+
+        public static readonly string Reset         = "\e[0m";
+        public static readonly string Bold          = "\e[0m";
+        public static readonly string Underline     = "\e[0m";
+        public static readonly string NoUnderline   = "\e[0m";
+        public static readonly string ReverseTest   = "\e[0m";
+        public static readonly string PositiveText  = "\e[0m";
+
+
+
+    internal static class Background
+    {
+
+        public static readonly string Black        = "\e[40m";
+        public static readonly string Red          = "\e[41m";
+        public static readonly string Green        = "\e[42m";
+        public static readonly string Yellow       = "\e[43m";
+        public static readonly string Blue         = "\e[44m";
+        public static readonly string Magenta      = "\e[45m";
+        public static readonly string Cyan         = "\e[46m";
+        public static readonly string LightGray    = "\e[47m";
+        public static readonly string Gray     = "\e[100m";
+        public static readonly string LightRed     = "\e[101m";
+        public static readonly string LightGreen   = "\e[102m";
+        public static readonly string LightYellow  = "\e[1033m";
+        public static readonly string LightBlue    = "\e[104m";
+        public static readonly string LightMagenta = "\e[105m";
+        public static readonly string LightCyan    = "\e[106m";
+        public static readonly string White        = "\e[107m";
+
+    }
+
+    internal static class Foreground
+    {
+
+        public static readonly string Black        = "\e[30m";
+        public static readonly string Red          = "\e[31m";
+        public static readonly string Green        = "\e[32m";
+        public static readonly string Yellow       = "\e[33m";
+        public static readonly string Blue         = "\e[34m";
+        public static readonly string Magenta      = "\e[35m";
+        public static readonly string Cyan         = "\e[36m";
+        public static readonly string LightGray    = "\e[37m";
+        public static readonly string DarkGray     = "\e[90m";
+        public static readonly string LightRed     = "\e[91m";
+        public static readonly string LightGreen   = "\e[92m";
+        public static readonly string LightYellow  = "\e[93m";
+        public static readonly string LightBlue    = "\e[94m";
+        public static readonly string LightMagenta = "\e[95m";
+        public static readonly string LightCyan    = "\e[96m";
+        public static readonly string White        = "\e[97m";
+
+    }
+    }
+}
